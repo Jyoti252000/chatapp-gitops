ce233555d84b4617e914ff204f3ef44a29da3486
Jyoti Giri <Jyotigiri25oct@gmail.com>
2026-09-13 02:35:26 +0100

    Fix EKS version to 1.36, add ArgoCD Application and gp3 StorageClass

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -25,7 +25,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster.arn
-  version  = "1.30"
+  version  = "1.36"
 
   vpc_config {
     subnet_ids = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
